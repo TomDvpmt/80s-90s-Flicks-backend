d6cea3ca0bf262275519f51a687218c1b5f1cf0c
TomDvpmt <tbsuiviactu@gmail.com>
2023-06-22 22:26:02 +0200

    Added: getDemoUserId route

--- controllers/configCtrl.js
+++ controllers/configCtrl.js
@@ -5,3 +5,7 @@ exports.initializeAPI = (req, res, next) => {
 exports.getImgBBAPIKey = (req, res, next) => {
     res.status(200).json(process.env.IMGBB_API_KEY);
 };
+
+exports.getDemoUserId = (req, res, next) => {
+    res.status(200).json(process.env.DEMO_USER_ID);
+};
--- routes/configRoutes.js
+++ routes/configRoutes.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const { auth } = require("../middleware/auth");
-const { initializeAPI, getImgBBAPIKey } = require("../controllers/configCtrl");
+const {
+    initializeAPI,
+    getImgBBAPIKey,
+    getDemoUserId,
+} = require("../controllers/configCtrl");
 
 router.get("/initialize", initializeAPI);
 router.get("/imgbb-api-key", auth, getImgBBAPIKey);
+router.get("/demo-user-id", auth, getDemoUserId);
 
 module.exports = router;
